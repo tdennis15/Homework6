afefc96191ce0d2398ec8443e82ecd748f42e751
TJ Dennis <tdennis15@mail.wou.edu>
2017-11-29 11:22:32 -0800

    update controller

--- HW6/Homework6/Homework6/Controllers/HomeController.cs
+++ HW6/Homework6/Homework6/Controllers/HomeController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Homework6.Models;
 
 namespace Homework6.Controllers
 {
     public class HomeController : Controller
     {
+        /// <summary>
+        /// Creating a context instance in so that we can access the database inside our controller
+        /// </summary>
+        private AdventureWorksContext db = new AdventureWorksContext();
+
+
+        /// <summary>
+        /// Standard index action handler. This will handle the landing page and the home page
+        /// </summary>
+        /// <returns></returns>
         public ActionResult Index()
         {
             return View();
